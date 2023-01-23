b59fad24b634e021becbff403f6de239302c6efd
Ori <ori.reiter@gmail.com>
2023-01-23 22:44:45 +0200

    start to scrape lobbyist names

---
+++ lobby_actions/logic/lobbyist_logic.py
@@ -0,0 +1,67 @@
+from playwright.sync_api import sync_playwright, Page
+
+
+class LobbyistNamesExtractor:
+    URL_WITH_LOBBYISTS_NAME_TO_SCRAPE = "https://main.knesset.gov.il/About/Lobbyist/Pages/Lobbyist.aspx"
+    LOBBYIST_TR_XPATH = '//tr[contains(@id, "Result")]'
+    GO_TO_NEXT_PAGE_XPATH = '//a[contains(@id, "Next")]'
+
+    def extract(self):
+        with sync_playwright() as playwright_object:
+            playwright_browser = playwright_object.firefox.launch()
+            page = playwright_browser.new_page()
+
+            lobbyist_names = self.scrape_all_lobbyists_names(playwright_page=page)
+            return lobbyist_names
+
+            # page.goto(url=self.URL_WITH_LOBBYISTS_NAME_TO_SCRAPE, wait_until='domcontentloaded')
+            # page.wait_for_load_state('networkidle')
+            #
+            # gabi = page.locator('xpath=//table[contains(@id, "LobbystResult")]').all()
+            # print(page.title())
+            # playwright_browser.close()
+
+    def scrape_all_lobbyists_names(self, playwright_page: Page):
+        playwright_page.goto(url=self.URL_WITH_LOBBYISTS_NAME_TO_SCRAPE, wait_until='domcontentloaded')
+        playwright_page.wait_for_load_state('networkidle')
+
+        names = set()
+        for page_index in range(10):
+            names.update(self.get_names_from_current_page(playwright_page=playwright_page))
+
+            next_button = playwright_page.locator(self.GO_TO_NEXT_PAGE_XPATH).first
+            if next_button.is_disabled():
+                break
+
+            next_button.click()
+            playwright_page.wait_for_load_state('domcontentloaded')
+            playwright_page.wait_for_load_state('networkidle')
+
+        return names
+
+    def get_names_from_current_page(self, playwright_page: Page):
+        table_rows_of_lobbyists = playwright_page.locator(self.LOBBYIST_TR_XPATH).all()
+
+        lobbyists_names = []
+
+        for table_row in table_rows_of_lobbyists:
+            name = table_row.locator('xpath=/td').first.text_content().strip()
+            lobbyists_names.append(name)
+
+        return lobbyists_names
+
+
+class LobbyistNamesTransformer:
+    def transform(self, raw_data):
+        return raw_data
+
+
+def get_lobbyists_from_etl():
+    raw_data = LobbyistNamesExtractor().extract()
+    transformed_data = LobbyistNamesTransformer().transform(raw_data=raw_data)
+    return transformed_data
+
+
+if __name__ == '__main__':
+    ori = get_lobbyists_from_etl()
+    x = 1
