59d6754052f5dda8d32c50b8e3e7bd13d28befa0
ori_reiter <ori@yaballe.com>
2023-01-29 21:09:31 +0200

    start parsing data for mail

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,26 @@
+import logging
+from logging.config import dictConfig
 from apscheduler.schedulers.background import BlockingScheduler
 
 from lobby_actions.utils import load_env_vars
+
 load_env_vars()
 
-from lobby_actions.logic.protocol_file_logic import KnessetProtocolsETL, KnessetProtocolTransformer
+from lobby_actions.config.logging_config import LOGGING_CONFIG
 from lobby_actions.logic.lobbyist_logic import get_lobbyists_from_etl
+from lobby_actions.logic.protocol_file_logic import (
+    KnessetProtocolsETL,
+    KnessetProtocolTransformer,
+)
+
+
+logger = logging.getLogger(__name__)
+
 
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('cron', day_of_week='sun', hour=7)
+@scheduler.scheduled_job("cron", day_of_week="sun", hour=7)
 def notify_lobbyists_actions_by_mail():
     try:
         lobbyists = get_lobbyists_from_etl()
@@ -19,5 +30,7 @@ def notify_lobbyists_actions_by_mail():
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    dictConfig(LOGGING_CONFIG)
+
     scheduler.start()
