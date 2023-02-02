9c2c2f030d1e325422ec2f4b3e5744f14342f1aa
Ori <ori.reiter@gmail.com>
2023-02-02 22:08:05 +0200

    formatter

--- lobby_actions/logic/protocol_file_logic.py
+++ lobby_actions/logic/protocol_file_logic.py
@@ -51,9 +51,9 @@ class KnessetProtocolTransformer:
     }
 
     def __init__(
-            self,
-            x_days_ago_as_datetime: datetime = datetime.utcnow() - timedelta(days=14),
-            lobbyists_to_check: list[str] = None,
+        self,
+        x_days_ago_as_datetime: datetime = datetime.utcnow() - timedelta(days=14),
+        lobbyists_to_check: list[str] = None,
     ):
         self._x_days_ago_as_datetime = x_days_ago_as_datetime
         self._field_names = []
@@ -112,7 +112,7 @@ class KnessetProtocolTransformer:
         return self._x_days_ago_as_datetime < protocol_line.start_date
 
     def _append_line_to_relevant_lobbyists_summary(
-            self, protocol_line: models.ProtocolLineDto
+        self, protocol_line: models.ProtocolLineDto
     ):
         invitees_names = {
             invitee_dict.get("name") for invitee_dict in protocol_line.invitees
@@ -159,12 +159,12 @@ class KnessetProtocolsETL:
     PROTOCOL_FILE_URL = "https://storage.googleapis.com/knesset-data-pipelines/data/people/committees/meeting-attendees/kns_committeesession.csv"
 
     def __init__(
-            self,
-            extractor: KnessetProtocolExtractor = KnessetProtocolExtractor(),
-            transformer: KnessetProtocolTransformer = KnessetProtocolTransformer(
-                x_days_ago_as_datetime=datetime.utcnow() - timedelta(days=7)
-            ),
-            loader: KnessetProtocolLoader = KnessetProtocolLoader(),
+        self,
+        extractor: KnessetProtocolExtractor = KnessetProtocolExtractor(),
+        transformer: KnessetProtocolTransformer = KnessetProtocolTransformer(
+            x_days_ago_as_datetime=datetime.utcnow() - timedelta(days=7)
+        ),
+        loader: KnessetProtocolLoader = KnessetProtocolLoader(),
     ):
         self._field_names = []
         self.extractor = extractor
--- lobby_actions/third_parties/gmail.py
+++ lobby_actions/third_parties/gmail.py
@@ -10,11 +10,11 @@ logger = logging.getLogger(__name__)
 
 class GmailSmtp:
     def __init__(
-            self,
-            smtp_server_url: str = os.environ.get("GMAIL_SMTP_SERVER_URL"),
-            smtp_server_port: int = int(os.environ.get("GMAIL_SMTP_SERVER_PORT")),
-            user_id: str = os.environ.get("GMAIL_SMTP_USER_ID"),
-            password: str = os.environ.get("GMAIL_SMTP_PASSWORD"),
+        self,
+        smtp_server_url: str = os.environ.get("GMAIL_SMTP_SERVER_URL"),
+        smtp_server_port: int = int(os.environ.get("GMAIL_SMTP_SERVER_PORT")),
+        user_id: str = os.environ.get("GMAIL_SMTP_USER_ID"),
+        password: str = os.environ.get("GMAIL_SMTP_PASSWORD"),
     ):
         self._smtp_server_url = smtp_server_url
         self._smtp_server_port = smtp_server_port
@@ -22,7 +22,7 @@ class GmailSmtp:
         self._password = password
 
     def send_mail(
-            self, receivers: list[str], subject: str, content: str, is_rtl: bool = False
+        self, receivers: list[str], subject: str, content: str, is_rtl: bool = False
     ):
         email_to_send = self._build_email(
             receivers=receivers, subject=subject, content=content, is_rtl=is_rtl
@@ -34,10 +34,12 @@ class GmailSmtp:
             server.ehlo()
 
             server.login(self._user_id, self._password)
-            server.sendmail(self._user_id, receivers, email_to_send.as_string().encode('UTF-8'))
+            server.sendmail(
+                self._user_id, receivers, email_to_send.as_string().encode("UTF-8")
+            )
 
     def _build_email(
-            self, receivers: list[str], subject: str, content: str, is_rtl: bool = False
+        self, receivers: list[str], subject: str, content: str, is_rtl: bool = False
     ):
         # Create the container email message.
         msg = EmailMessage()
@@ -49,8 +51,8 @@ class GmailSmtp:
 
         if content and is_rtl:
             content = f'<div style="direction:rtl">{content}</div>'
-            content = content.replace('\n', '<br>')
-            msg.add_header('Content-Type', 'text/html')
+            content = content.replace("\n", "<br>")
+            msg.add_header("Content-Type", "text/html")
 
         logger.info(
             f"sending from {self._user_id} to {receivers=} {subject=} {content=}"
