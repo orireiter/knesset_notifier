a2fe6999f130d55886f4419e01cb6225dc73b556
Ori <ori.reiter@gmail.com>
2023-02-09 23:32:12 +0200

    start optimizing env conf

---
+++ lobby_actions/config/env_conf.py
@@ -0,0 +1,43 @@
+import os
+
+from dotenv import load_dotenv
+
+from lobby_actions.data.models import ConfigModels
+
+
+class EnvironmentConfig:
+    _instance = None
+    _is_instance_initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance:
+            return cls._instance
+
+        cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if self._is_instance_initialized:
+            return
+
+        self.load_env_vars()
+
+        # init values
+        self.gmail_smtp = ConfigModels.GmailSmtp(
+            server_url=os.environ.get("GMAIL_SMTP_SERVER_URL", ''),
+            server_port=os.environ.get("GMAIL_SMTP_SERVER_PORT", ''),
+            user_id=os.environ.get("GMAIL_SMTP_USER_ID", ''),
+            password=os.environ.get("GMAIL_SMTP_PASSWORD", ''),
+        )
+
+        self._is_instance_initialized = True
+
+    @staticmethod
+    def load_env_vars():
+        if os.path.exists("env.py"):
+            import env
+
+            return env.load_env_from_config()
+
+        load_dotenv()
+
